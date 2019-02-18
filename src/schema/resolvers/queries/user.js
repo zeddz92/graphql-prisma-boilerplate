a082cf80d6f807c49e38af005b9fc7783e60e8cd
edwardr <lopezredd@gmail.com>
2019-02-18 18:47:30 -0400

    Updated query to fetch data using the getQuery util

--- src/schema/resolvers/queries/user.js
+++ src/schema/resolvers/queries/user.js
@@ -1,25 +1,9 @@
-function get(obj) {
-
-}
+import {rootQuery} from '../../../utils/getQuery';
 
 const user = {
 
     async users(parent, args, {prisma}, info) {
-
-        const {where, orderBy} = args;
-
-        const opArgs = {
-            ...args.pagination,
-            where,
-            orderBy
-        };
-
-        const nodes = await prisma.query.users(opArgs);
-
-        return {
-            nodes,
-            pageInfo: args.pageInfo
-        }
+        return rootQuery(args, info.fieldName);
     },
 
 };
