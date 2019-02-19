609f54c5fe36ff128c42d431636b4a19f2884622
edwardr <lopezredd@gmail.com>
2019-02-19 00:40:26 -0400

    Remove password length validation

--- src/utils/hashPassword.js
+++ src/utils/hashPassword.js
@@ -1,11 +1,7 @@
 import bcrypt from 'bcryptjs'
 
 const hashPassword = (password) => {
-    if(password.length < 6) {
-        throw new Error('Password must be 6 characters or longer.')
-    }
-
-    return bcrypt.hash(password, 10)
-}
+    return bcrypt.hash(password, 10);
+};
 
 export {hashPassword as default}
